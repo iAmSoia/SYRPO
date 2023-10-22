d4f4036bf956b21d47918291a2e0efa74d5c4629
iAmSoia <viy_st@bk.ru>
2023-10-22 12:27:24 +0300

    new method

--- 4лаба/4лаба/Program.cs
+++ 4лаба/4лаба/Program.cs
@@ -5,10 +5,14 @@ class Programm
     static void Main(string[] args)
     {
         WorkWithUser workWithUser = new();
+        workWithUser.WriteHi();
     }
 }
 
 class WorkWithUser
 {
-
+    public void WriteHi()
+    {
+        Console.WriteLine("Hi!");
+    }
 }
