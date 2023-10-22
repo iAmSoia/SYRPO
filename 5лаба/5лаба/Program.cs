b2d01321f36e7928c3b6cf48a1dbb6fcb546eaa5
iAmSoia <viy_st@bk.ru>
2023-10-22 19:27:04 +0300

    методы калькулятора

--- 5лаба/5лаба/Program.cs
+++ 5лаба/5лаба/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+using System.ComponentModel;
+
+namespace Laba5
+{
+    public class Programm
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+    public class Calcultor
+    {
+        public void Add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void Subtract(int a, int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        public void Multiply(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        public void Devide(int a, int b)
+        {
+            Console.WriteLine(a / b);
+        }
+    }
+    
+}
+
+
+
